0e801f57cd9d882a9a8b51fbcd8c83fe06772822
callmejeje <choije401@naver.com>
2025-12-02 14:43:48 +0900

    feat: 입력창 늘어나게 수정 및 작성 중에 나갈 때 알림

--- src/features/diary/screens/DiaryCreateScreen/index.tsx
+++ src/features/diary/screens/DiaryCreateScreen/index.tsx
@@ -1,10 +1,12 @@
 import CommonHeader from "@/components/CommonHeader";
+import KeyboardAvoidingWrapper from "@/components/KeyboardAvoidingWrapper";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { X } from "lucide-react-native";
 import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
-import { Button, H4, YStack } from "tamagui";
+import { Alert } from "react-native";
+import { Button, H4, ScrollView, YStack } from "tamagui";
 import DiaryForm from "./components/DiaryForm";
 import DiaryImagePicker from "./components/DiaryImagePicker";
 import { DiaryInput, DiarySchema } from "./schemas/diary.schema";
@@ -21,30 +23,58 @@ export default function DiaryCreateScreen() {
     defaultValues: { content: "" },
   });
 
-  return (
-    <FormProvider {...methods}>
-      <CommonHeader
-        left={
-          <Button
-            icon={X}
-            onPress={() => navigation.goBack()}
-            chromeless
-            pressStyle={{
-              scale: 0.9,
-              opacity: 0.7,
-              backgroundColor: "transparent",
-              borderColor: "transparent",
-            }}
-            padding={"$2"}
-          />
-        }
-        center={<H4>다이어리 작성</H4>}
-      />
-      <YStack padding={20} gap={12} flex={1}>
-        <DiaryImagePicker imageUri={imageUri} setImageUri={setImageUri} />
+  const handleBackPress = () => {
+    const content = methods.watch("content");
+    if (content) {
+      Alert.alert(
+        "작성 중인 내용이 있습니다.",
+        "정말로 나가시겠습니까? 변경사항은 저장되지 않습니다.",
+        [
+          {
+            text: "취소",
+            style: "cancel",
+          },
+          {
+            text: "나가기",
+            onPress: () => navigation.goBack(),
+            style: "destructive",
+          },
+        ]
+      );
+    } else {
+      navigation.goBack();
+    }
+  };
 
-        <DiaryForm groupId={groupId} imageUri={imageUri} />
+  return (
+    <KeyboardAvoidingWrapper>
+      <YStack flex={1}>
+        <CommonHeader
+          left={
+            <Button
+              icon={X}
+              onPress={handleBackPress}
+              chromeless
+              pressStyle={{
+                scale: 0.9,
+                opacity: 0.7,
+                backgroundColor: "transparent",
+                borderColor: "transparent",
+              }}
+              padding={"$2"}
+            />
+          }
+          center={<H4>일기 작성</H4>}
+        />
+        <FormProvider {...methods}>
+          <ScrollView contentContainerStyle={{ flex: 1 }}>
+            <YStack padding={20} gap={12} flex={1}>
+              <DiaryImagePicker imageUri={imageUri} setImageUri={setImageUri} />
+              <DiaryForm groupId={groupId} imageUri={imageUri} />
+            </YStack>
+          </ScrollView>
+        </FormProvider>
       </YStack>
-    </FormProvider>
+    </KeyboardAvoidingWrapper>
   );
 }
